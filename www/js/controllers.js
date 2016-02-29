a91e8fa2062c0b70e4c0917768860f2f672ccb4e
antxelena50 <antxelena50@users.noreply.github.com>
2016-02-29 15:54:48 +0100

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -24,7 +24,7 @@ $JSView.controller = {
     Tornada: function(e){
 		$JSView.dataView({},e)
 	},
-    modalA: function(e){
-		$JSView.dataView({},e)	
+    Menú: function(e){
+		$JSView.declareModal({},e)
 	}
 }
